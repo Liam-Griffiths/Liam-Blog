b98264c14b47f7b46c93071b895b1dc9cc57d448
Liam Griffiths <liam@liam-griffiths.co.uk>
2025-08-03 00:25:15 -0700

    Update styling of Software Engineer title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
           <div className="space-y-4 py-1 text-base leading-7 sm:landscape:space-y-2 sm:landscape:leading-3 sm:mx-4">
             <article className="prose lg:prose-xl text-white sm:landscape:prose-md">
               <div className="text-center items-center md:m-4">
-                <h1 className="mt-2 text-6xl font-bold tracking-tight text-black dark:text-white text-left px-7 mb-0">Liam<br/>Griffiths<br/><div className="font-light text-gray-700 dark:text-black underline mt-2" >Software<br/>Engineer<span className="font-bold ml-1 blinking-cursor">|</span></div></h1>
+                <h1 className="mt-2 text-6xl font-bold tracking-tight text-black dark:text-white text-left px-7 mb-0">Liam<br/>Griffiths<br/><div className="font-light text-gray-700 dark:text-black mt-2" ><span className="underline">Software<br/>Engineer</span><span className="font-bold ml-1 blinking-cursor">|</span></div></h1>
                 <p className="text-lg leading-8 tracking-tight text-justify px-7 mt-4 text-gray-700 dark:text-white">Engineer from the UK. Living in BC, Canada. Builder of robust clean codebases. Lover of guitar, audiobooks, writing and hiking.</p>
                 <div className="flex justify-center space-x-4 mt-4">
                   <a href="https://github.com/Liam-Griffiths" target="_blank" rel="noopener noreferrer" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-0.5 px-2 rounded-full transition duration-300 ease-in-out shadow-angled-left-down dark:bg-blue-500 dark:hover:bg-blue-600 dark:text-white">
